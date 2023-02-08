75e1d2b4cf4f57a841a3075b1913753a272ed10b
santhoshmural <120946243+santhoshmural@users.noreply.github.com>
2023-02-08 21:17:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", "11319890"))
 API_HASH = environ.get("API_HASH", "4965e574411647c42ffbd8b46040f0a5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5670197186:AAHGpfSiCrK7kXCo3M03vyg7FuTn1tX9UDI")
 ADMIN = int(environ.get("ADMIN", "1365582907"))          
-CAPTION = environ.get("CAPTION", "@Tamildubbbedpublic")
+CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "https://te.legra.ph/file/1d911704c0de0b62a7d77.jpg")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://te.legra.ph/file/b1eeafba752be7cfb39df.jpg")
